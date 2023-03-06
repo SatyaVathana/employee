d2a4362f40cc28d86e90f2b45a5fbf1b20af4862
SatyaVathana <103138748+SatyaVathana@users.noreply.github.com>
2023-03-06 13:46:39 +0700

    format code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import EmpListing from './EmpListing';
-import EmpCreate from './EmpCreate';
-import EmpDetail from './EmpDetail';
-import EmpEdit from './EmpEdit';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./EmpListing";
+import EmpCreate from "./EmpCreate";
+import EmpDetail from "./EmpDetail";
+import EmpEdit from "./EmpEdit";
 
 function App() {
   return (
@@ -12,16 +12,15 @@ function App() {
       <h1>React JS CRUD Opertations</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<EmpListing />}></Route>
-          <Route path='/employee/create' element={<EmpCreate />}></Route>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path="/employee/create" element={<EmpCreate />}></Route>
 
-          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
-          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+          <Route path="/employee/detail/:empid" element={<EmpDetail />}></Route>
+          <Route path="/employee/edit/:empid" element={<EmpEdit />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
-
 }
 
 export default App;
